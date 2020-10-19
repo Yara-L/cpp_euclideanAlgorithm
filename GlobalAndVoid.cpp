de7db3961fca8e7d987bb92d67c5037aed036012
Yaroslava L <66276044+Yara-L@users.noreply.github.com>
2020-10-19 19:22:26 +0300

    Euclidean Algorithm with VOID and GLOBAL variable
    This Euclidean algorithm is written and made by using VOID and a global variable
    Also we can see that I used library time.h and clocks so we can see how fast the program compiles

---
+++ GlobalAndVoid.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+long int glob;
+void gdc(long long x, long long y);
+
+int main() {
+	clock_t c0, c1, c2, c3;
+	long long x = 12586269025, y = 1836311903;
+
+	c0 = clock();
+	gdc(x, y);
+	cout << "\nGDC of " << x << " and " << y << " is " << endl;
+	c1 = clock();
+
+	float time1 = (float)(c1 - c0) / CLOCKS_PER_SEC;
+	cout << "\nTIME: " << time1 << endl;
+
+	return 0;
+}
+
+void gdc(long long x, long long y) {
+	if (x % y == 0) {
+		glob = y;
+		cout << "GDC ( " << x << " , " << y << " )" << endl;
+	}
+	else if (x > y) {
+		cout << "GDC ( " << x << " , " << y << " )" << endl;
+		return gdc(x % y, y);
+	}
+	else if (x < y) {
+		cout << "GDC ( " << x << " , " << y << " )" << endl;
+		return gdc(y % x, x);
+	}
+
+}
